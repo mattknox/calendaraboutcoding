15f388edafeb7dd77f18e8ba542023ac3ca3963e
matt knox <matthewknox@gmail.com>
2008-10-29 22:26:13 -0400

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+  def seinfeld
+    now        = Date.new(params[:year], params[:month])
+    prev_month = now << 1
+    next_month = now >> 1
+    calendar :year => now.year, :month => now.month,
+    :previous_month_text => %(<a href="/~#{self.login}/#{prev_month.year}/#{prev_month.month}">Previous Month</a>), 
+    :next_month_text     => %(<a href="/~#{self.login}/#{next_month.year}/#{next_month.month}" class="next">Next Month</a>) do |d|
+#      if @progressions.include? d
+#        [d.mday, {:class => "progressed"}]
+#      else
+#        [d.mday, {:class => "slacked"}]
+#      end
+    end
+  end
 end
