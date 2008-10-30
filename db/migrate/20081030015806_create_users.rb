e492041c7b02f4a296fed33334d3f8e687e28f69
matt knox <matthewknox@gmail.com>
2008-10-29 23:03:24 -0400

    removed denormalization from users

--- db/migrate/20081030015806_create_users.rb
+++ db/migrate/20081030015806_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :name
-      t.integer :streak
-      t.integer :max_streak
 
       t.timestamps
     end
