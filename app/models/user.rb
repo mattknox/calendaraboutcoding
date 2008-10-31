4d7904fe82fc2f289b2f2b5831fdba7340dd2051
matt knox <matthewknox@gmail.com>
2008-10-30 23:38:30 -0400

    removed dummy function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,6 @@ class User < ActiveRecord::Base
   has_many :days
 
   def on_days
-    (17..29).map { |i| Date.today.beginning_of_month + i.days }
-  end
-  
-  def seinfeld
-    now        = Date.new(2008, 10 )
-    prev_month = now << 1
-    next_month = now >> 1
-    calendar :year => now.year, :month => now.month,
-    :previous_month_text => %(<a href="/~#{self.name}/#{prev_month.year}/#{prev_month.month}">Previous Month</a>), 
-    :next_month_text     => %(<a href="/~#{self.name}/#{next_month.year}/#{next_month.month}" class="next">Next Month</a>) do |d|
-      logger.info d.inspect
-      if self.on_days.include? d
-        [d.mday, {:class => "progressed"}]
-      else
-        [d.mday, {:class => "slacked"}]
-      end
-    end
+    self.days.map { |d| d.date }
   end
 end
