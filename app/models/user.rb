cccbdc32fca6e5aa3f5eca1a5c7725cdb8221614
matt knox <matthewknox@gmail.com>
2008-11-26 18:55:13 -0500

    added feed_me to grab stuff out of github, and the skeleton for pull-history-from-github.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   has_many :checkins
   has_many :days
 
+  after_create :pull_all_history
+
+  def pull_history
+    # here, we should pull all of history, back as far as one's most recent commits
+  end
+  
   def on_days
     self.days.map { |d| d.date }
   end
