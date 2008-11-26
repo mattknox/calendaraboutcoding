2b051e5b2ee7b0f3511a300561cb1322e063fbe3
matt knox <matthewknox@gmail.com>
2008-11-26 14:07:35 -0500

    removed default routes

--- config/routes.rb
+++ config/routes.rb
@@ -40,10 +40,4 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "users"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
